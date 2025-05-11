77b187fa0067517a9f88f0315898e096581b1c74
El khadir Safouane <elkhadirsafouane@gmail.com>
2025-05-11 15:13:03 +0100

    added 2nd ml model

--- app.py
+++ app.py
@@ -4,10 +4,35 @@ import joblib
 
 app = Flask(__name__)
 model = joblib.load("model/heart_model.pkl")
+model2 = joblib.load("model/heart_model_reduced.pkl")
 
-@app.route('/')
+
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict2():
+    # Extract features from form
+    features = [float(request.form[feature]) for feature in [
+        'cp', 'thalach', 'ca', 'oldpeak', 'thal', 'age']]
+
+    # Reshape for prediction
+    features_array = np.array(features).reshape(1, -1)
+
+    # Make prediction
+    prediction = model2.predict(features_array)[0]
+
+    result = "has a high risk of heart disease." if prediction == 1 else "has a low risk of heart disease."
+
+    return render_template('index2.html', prediction_text=f"The patient {result}")
+
+
+@app.route('/index')
 def home():
     return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -35,6 +60,7 @@ def predict():
         return render_template('index.html', result=result, probability=f"{prob:.2f}%")
     except Exception as e:
         return str(e)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
